ec3bd26e10f91f625b8d3410e90aff11e9560c6c
Angel <angelecheverriah@gmail.com>
2026-02-13 17:54:07 -0400

    se agregaron codigo en los archivos login.ts y login.tsx

--- Login-project-backend/routes/login.js
+++ Login-project-backend/routes/login.js
@@ -1,7 +1,27 @@
+const { jsonResponse } = require("../conexion/jsonResponse");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("login");
+router.post("/", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json(
+      jsonResponse(400, {
+        error: "Fields are required",
+      }),
+    );
+  }
+
+  const accessToken = "access_token";
+  const refreshToken = "refresh_token";
+  const user = {
+    id: "1",
+    username: "Joe Don",
+    password: "XXXXXXX",
+  };
+
+  res.status(200).json(jsonResponse(200, { user, accessToken, refreshToken }));
 });
 
 module.exports = router;
